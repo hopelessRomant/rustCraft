e494eb96d2221ac76b3bc35b5e3d9bd7b78e99dc
hopelessRomant <satvolume@gmail.com>
2025-10-13 20:51:11 +0000

    part 1

--- guessgame/src/main.rs
+++ guessgame/src/main.rs
@@ -7,5 +7,7 @@ fn main(){
     io::stdin()
         .read_line(&mut guess) // & is used for declairing reference, mut makes it mutable
         .expect("Failed to read input");
-    
+    // 2 possible variants for the enum 'Result' returned by the io::stdin()
+
+    println!("guessed number : {guess}");
 }
