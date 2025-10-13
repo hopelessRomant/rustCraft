09a4b02022897a2a506ca3bbfb694fa17f9bc18d
hopelessRomant <satvolume@gmail.com>
2025-10-13 20:15:15 +0000

    std io

--- guessgame/src/main.rs
+++ guessgame/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main(){
+    println!("input your guess");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess) // & is used for declairing reference, mut makes it mutable
+        .expect("Failed to read input");
+    
 }
