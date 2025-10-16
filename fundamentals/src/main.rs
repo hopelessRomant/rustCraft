ac0c7aab4c7ca502fd9bf7fd67eea5d41c1f09c3
hopelessRomant <satvolume@gmail.com>
2025-10-16 19:27:41 +0000

    statement and expression

--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -1,8 +1,33 @@
 // Functions
 fn main() {
     print_labeled_measurement(5, 'h');
+    eg1();
+    eg2();
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+// statements and expressions
+//Statements are instructions that perform some action and do not return a value.
+//Expressions evaluate to a resultant value.
+
+fn eg1() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+}
+
+fn five() -> i32 {
+    5 // no semicolon to declare it as a statement.
+}
+
+fn eg2() {
+    let x = five();
+
+    println!("The value of x is: {x}");
 }
