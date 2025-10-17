14fd6e91645a5074ec3686f3b4c1b276e66c1443
hopelessRomant <satvolume@gmail.com>
2025-10-17 21:50:17 +0000

    break return

--- chapter3/loops/src/main.rs
+++ chapter3/loops/src/main.rs
@@ -1,3 +1,17 @@
+// rust has 3 loops 
+// * loop
+// * for
+// * while 
+
+//passing result out of a loop
 fn main() {
-    println!("Hello, world!");
+    let mut counter = 0;
+    let number = loop {
+        counter += 1;
+        if counter == 5 {
+            break counter*2; // return value 10
+        }
+    };
+    println!("{number}")
 }
+
