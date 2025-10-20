1840ca43ae6479bd5a08a5981eac183726497beb
hopelessRomant <satvolume@gmail.com>
2025-10-20 22:36:24 +0000

    values on stack

--- chapter4/src/main.rs
+++ chapter4/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // Strings store value in Heap 
     let mut s = String::from("Hello"); // Strings can be mutated since they use heaps not stacks
     s.push_str(", world!!");
     println!("{s}");
@@ -13,4 +14,9 @@ fn main() {
     let s1 = String::from("alice");
     let s2 = s1.clone();
     println!("s1 = {s1}, s2 = {s2}");
+
+    // int types generally store values on stack
+    let x = 5;
+    let y = x;
+    println!("x = {x}, y = {y}");
 }
