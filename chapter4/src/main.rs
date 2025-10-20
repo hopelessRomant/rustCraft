7c45499eb18c046df51bca1c34b2e68232c34e15
hopelessRomant <satvolume@gmail.com>
2025-10-20 22:24:25 +0000

    double free error

--- chapter4/src/main.rs
+++ chapter4/src/main.rs
@@ -2,4 +2,10 @@ fn main() {
     let mut s = String::from("Hello"); // Strings can be mutated since they use heaps not stacks
     s.push_str(", world!!");
     println!("{s}");
+
+    // double free error
+    let s1 = String::from("sahil");
+    let s2 = s1;
+    // println!("{s1}"); --> `s1` has type `String`, which does not implement the `Copy` trait
+    println!("{s2}");
 }
