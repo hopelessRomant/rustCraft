fbeb9e2d1e214469ac823d8361c3c788971f02a6
hopelessRomant <satvolume@gmail.com>
2025-10-20 22:32:45 +0000

    clone

--- chapter4/src/main.rs
+++ chapter4/src/main.rs
@@ -6,6 +6,11 @@ fn main() {
     // double free error
     let s1 = String::from("sahil");
     let s2 = s1;
-    // println!("{s1}"); --> `s1` has type `String`, which does not implement the `Copy` trait
+    // println!("{s1}"); --> `s1` has type `String`, which does not implement the `Copy` trait, it 'moves' the value
     println!("{s2}");
+
+    // clone method
+    let s1 = String::from("alice");
+    let s2 = s1.clone();
+    println!("s1 = {s1}, s2 = {s2}");
 }
