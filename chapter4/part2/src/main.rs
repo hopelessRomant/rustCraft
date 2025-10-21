46594b7c0ca4e4dac3ae52b91a17904c1bb241bc
hopelessRomant <satvolume@gmail.com>
2025-10-21 20:33:08 +0000

    multiple references

--- chapter4/part2/src/main.rs
+++ chapter4/part2/src/main.rs
@@ -5,9 +5,20 @@ fn main() {
 
     println!("size of {s1} is {num}");
 
-// mutable reference
+// mutable reference --> there can only be one mutable reference to a value in a given scope
     change(&mut s1);
     println!("{s1}");
+
+    {
+        let r1 = &mut s1;
+        r1.push_str(" -> first edit");
+        println!("{r1}");
+    }
+    {
+        let r2 = &mut s1;
+        r2.push_str(" -> second edit");
+        println!("{r2}");
+    }
 }
 
 fn calc_lenght(s : &String) -> usize {
