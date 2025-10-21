d91ddee82fddee7298d40542e6cacbab63466584
hopelessRomant <satvolume@gmail.com>
2025-10-21 20:21:54 +0000

    mutable reference

--- chapter4/part2/src/main.rs
+++ chapter4/part2/src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
 // references and borrowing
-    let s1 = String::from("gravity");
+    let mut s1 = String::from("gravity");
     let num = calc_lenght(&s1);
 
     println!("size of {s1} is {num}");
+
+// mutable reference
+    change(&mut s1);
+    println!("{s1}");
 }
 
 fn calc_lenght(s : &String) -> usize {
     s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str(" is changed");
 }
