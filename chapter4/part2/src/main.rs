5f7ba907383a0512afd14ad985a7d0cb3599aeeb
hopelessRomant <satvolume@gmail.com>
2025-10-21 18:40:52 +0000

    borrowing

--- chapter4/part2/src/main.rs
+++ chapter4/part2/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
 // references and borrowing
-    
+    let s1 = String::from("gravity");
+    let num = calc_lenght(&s1);
+
+    println!("size of {s1} is {num}");
+}
+
+fn calc_lenght(s : &String) -> usize {
+    s.len()
 }
