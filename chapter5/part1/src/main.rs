b2f124be0ac8367082a87a4c9d5530e88899673c
hopelessRomant <satvolume@gmail.com>
2025-10-23 20:38:31 +0000

    strunct update method2

--- chapter5/part1/src/main.rs
+++ chapter5/part1/src/main.rs
@@ -7,18 +7,25 @@ struct Student {
 }
 
 fn main() {
-    let mut s1 = Student{
+    let mut s1 = Student {
         name: String::from("Sahil"),
         roll: 22174019,
         department: String::from("Physics"),
         email: String::from("sahil@something.com"),
     };
+    s1 = Student {
+        roll: 22174018,
+        ..s1
+    };
 // changing roll
-    s1 = new_roll(2214018, s1);
+    // s1 = new_roll(2214018, s1);
     let roll = s1.roll;
     println!("{roll}")
+
+
 }
 
+#[allow(dead_code)]
 fn new_roll(roll: u32, mut s: Student) -> Student {
     s.roll = roll;
     s
