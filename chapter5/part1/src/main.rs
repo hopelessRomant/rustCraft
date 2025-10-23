56d30f307cc1da1718ab26b621b675ae0789119f
hopelessRomant <satvolume@gmail.com>
2025-10-23 20:32:26 +0000

    mut struct

--- chapter5/part1/src/main.rs
+++ chapter5/part1/src/main.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 struct Student {
     name: String,
     roll: u32,
@@ -6,5 +7,19 @@ struct Student {
 }
 
 fn main() {
-    
+    let mut s1 = Student{
+        name: String::from("Sahil"),
+        roll: 22174019,
+        department: String::from("Physics"),
+        email: String::from("sahil@something.com"),
+    };
+// changing roll
+    s1 = new_roll(2214018, s1);
+    let roll = s1.roll;
+    println!("{roll}")
+}
+
+fn new_roll(roll: u32, mut s: Student) -> Student {
+    s.roll = roll;
+    s
 }
