b97aa39cfe65664d2d389a2f95f1d499337bb869
hopelessRomant <satvolume@gmail.com>
2025-10-24 22:06:37 +0000

    function methods

--- chapter5/part3/src/main.rs
+++ chapter5/part3/src/main.rs
@@ -10,6 +10,16 @@ impl Rectangle {
     fn area(&self) -> u32{
         self.length * self.width
     }
+
+    fn will_fit(&self, test: &Rectangle) -> bool {
+        self.width > test.width && self.length > test.length
+    }
+
+    fn sq(size: u32) -> Rectangle {
+        Self { length: size, 
+            width: size, 
+        }
+    }
 }
 
 fn main() {
@@ -17,5 +27,18 @@ fn main() {
         length: 10,
         width: 20,
     };
-     println!("area of the rectangle is : {}", rect1.area())
+
+    let rect2 = Rectangle {
+        length: 5,
+        width: 10,
+    };
+
+    let sq1 = Rectangle::sq(20);
+
+    println!("area of the rectangle is : {}", rect1.area());
+    dbg!(&rect1);
+    println!("will rect2 fit in rect1: {}", rect1.will_fit(&rect2));
+    dbg!(&sq1);
+    println!("will sq1 fit in rect1: {}", rect1.will_fit(&sq1));
 }
+
