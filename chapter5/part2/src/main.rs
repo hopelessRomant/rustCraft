c6d835a7a76bb1d53727ec300d00e1b6ee6fb2b3
hopelessRomant <satvolume@gmail.com>
2025-10-24 21:15:52 +0000

    pretty printing

--- chapter5/part2/src/main.rs
+++ chapter5/part2/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     length: u32,
     bredth: u32,
@@ -9,6 +10,11 @@ fn main() {
         bredth: 10,
     };
     println!("the area of the rectangle is {}", area(&rect1));
+
+// pretty printing / debugging methods
+    println!("rect1 1 is {rect1:?}");
+    println!("rect1 1 is {rect1:#?}");
+    dbg!(&rect1); // returns ownership, so it can be used in expressions as well
 }
 
 fn area(rect: &Rectangle) -> u32 {
