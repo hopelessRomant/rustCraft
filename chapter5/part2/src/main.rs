ec3c797e401adcfb0a2a551cb29392269460298b
hopelessRomant <satvolume@gmail.com>
2025-10-24 21:10:40 +0000

    area calc

--- chapter5/part2/src/main.rs
+++ chapter5/part2/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+struct Rectangle {
+    length: u32,
+    bredth: u32,
 }
+
+fn main() {
+    let rect1 = Rectangle {
+        length: 20,
+        bredth: 10,
+    };
+    println!("the area of the rectangle is {}", area(&rect1));
+}
+
+fn area(rect: &Rectangle) -> u32 {
+    rect.length * rect.bredth
+}
+
