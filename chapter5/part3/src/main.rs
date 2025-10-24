deb9443ff036f5ebcb06e402a2109de639ec295c
hopelessRomant <satvolume@gmail.com>
2025-10-24 21:50:01 +0000

    area

--- chapter5/part3/src/main.rs
+++ chapter5/part3/src/main.rs
@@ -1,3 +1,21 @@
+// using methods to calculate area of reactangle
+
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32{
+        self.length * self.width
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        length: 10,
+        width: 20,
+    };
+     println!("area of the rectangle is : {}", rect1.area())
 }
