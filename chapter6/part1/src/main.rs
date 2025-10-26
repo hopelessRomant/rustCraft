f42ca6a677852aca87a9076b46283e4d2a1760ba
hopelessRomant <satvolume@gmail.com>
2025-10-26 18:31:03 +0000

    IP enum

--- chapter6/part1/src/main.rs
+++ chapter6/part1/src/main.rs
@@ -1,9 +1,10 @@
-enum Food {
-    Good,
-    Bad,
+#[allow(dead_code)]
+enum IpAdress {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 fn main() {
-    let _dish_a = Food::Bad;
-    let _dish_b = Food::Good;
-} 
+    let _primary = IpAdress::V4(127, 0, 0, 1);
+    let _sec = IpAdress::V6(String::from("::1"));
+}
