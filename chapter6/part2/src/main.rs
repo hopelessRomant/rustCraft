cd5ff913185822e4d5c5d4613e7e4d3b5ef4661b
hopelessRomant <satvolume@gmail.com>
2025-10-28 09:26:18 +0000

    Toss enum

--- chapter6/part2/src/main.rs
+++ chapter6/part2/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#[allow(dead_code)]
+enum Face {
+    Heads,
+    Tails,
+}
+
+#[allow(dead_code)]
+enum Color {
+    Red,
+    Blue,
+    Green,
+    Black,
+}
+
+#[allow(dead_code)]
+enum Toss {
+    Coin(Face),
+    Dice(Color),
+}
+
+fn main() {   
 }
