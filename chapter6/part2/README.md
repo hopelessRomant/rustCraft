100683093293afbf16ca8e878bb07373e3355162
hopelessRomant <satvolume@gmail.com>
2025-10-28 18:57:12 +0000

    cleaning

--- chapter6/part2/README.md
+++ chapter6/part2/README.md
@@ -3,3 +3,9 @@
 * Rust has an extremely powerful control flow construct called `match` that allows you to compare a value against a series of patterns and then execute code based on which pattern matches. Patterns can be made up of literal values, variable names, wildcards, and many other things; Chapter 19 covers all the different kinds of patterns and what they do. The power of `match` comes from the expressiveness of the patterns and the fact that the compiler confirms that all possible cases are handled.
 
 * Think of a `match` expression as being like a coin-sorting machine: coins slide down a track with variously sized holes along it, and each coin falls through the first hole it encounters that it fits into. In the same way, values go through each pattern in a `match`, and at the first pattern the value “fits,” the value falls into the associated code block to be used during execution.
+
+* I have created a mini tossing app using `match` and `enum`, which automatically returns a coin toss result and a die roll result
+
+```
+Ps. Die used is a Pyramid with coloured sides: Red, Blue, Green and Black
+```
--- chapter6/part2/src/main.rs
+++ chapter6/part2/src/main.rs
@@ -14,35 +14,35 @@ enum Color {
     Black,
 }
 
-enum Toss {
+enum Show {
     Coin(Face),
     Dice(Color),
 }
 
-fn result (toss: Toss) {
-    match toss {
-        Toss::Coin(face) => {println!("Result of the coin toss: {face:?}")}
-        Toss::Dice(color) => {println!("Result of die roll: {color:?}")}
+fn result (show: Show) {
+    match show {
+        Show::Coin(face) => {println!("Result of the coin toss: {face:?}")}
+        Show::Dice(color) => {println!("Result of die roll: {color:?}")}
     }
 }
 
-fn spin (r: &i32) -> Toss {
+fn spin (r: &i32) -> Show {
     match r {
-        0 => {Toss::Coin(Face::Heads)}
-        1 => {Toss::Coin(Face::Tails)}
-        2 => {Toss::Dice(Color::Red)}
-        3 => {Toss::Dice(Color::Green)}
-        4 => {Toss::Dice(Color::Blue)}
-        _ => {Toss::Dice(Color::Black)}
+        0 => {Show::Coin(Face::Heads)}
+        1 => {Show::Coin(Face::Tails)}
+        2 => {Show::Dice(Color::Red)}
+        3 => {Show::Dice(Color::Green)}
+        4 => {Show::Dice(Color::Blue)}
+        _ => {Show::Dice(Color::Black)}
     }
 }
 
 fn main() {
     let r1 = (rand::thread_rng().gen_range(1..=100)) %2;
     let r2 = (rand::thread_rng().gen_range(1..=100)) %4 + 2;
-    let coin_toss = spin(&r1);
-    let die_toss = spin(&r2);
+    let coin_show = spin(&r1);
+    let die_show = spin(&r2);
 
-    result(coin_toss);
-    result(die_toss);
+    result(coin_show);
+    result(die_show);
 }
