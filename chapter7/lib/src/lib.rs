868f7d2bf690389a0150e64ae4b1ab70fbbdd455
hopelessRomant <satvolume@gmail.com>
2025-11-03 23:23:01 +0000

    enum

--- chapter7/lib/src/lib.rs
+++ chapter7/lib/src/lib.rs
@@ -38,6 +38,12 @@ pub mod back_of_house{
         pub fn monday (drink: &str) -> Breakfast {
             Breakfast { drink: String::from(drink), special: String::from("red-velvet sunday") }
         }
+    }
+
+    #[derive(Debug)]
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }   
 }
 
--- chapter7/lib/src/main.rs
+++ chapter7/lib/src/main.rs
@@ -1,8 +1,12 @@
 use lib::back_of_house;
+use lib::back_of_house::Appetizer;
 use lib::eat_at_restraunt;
 
 fn main () {
     let meal = back_of_house::Breakfast::monday("chai");
     eat_at_restraunt();
     print!("{:#?}\n", meal); // the meal in the 'eat_at_restraunt function and the 'meal' defined in main are different things.
+
+    let starter = Appetizer::Salad;
+    println!("Starter order : {:?}", starter);
 }
