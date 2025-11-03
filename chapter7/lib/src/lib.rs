6c3509f28d282538229fae226a5e1d7ddff9e6bc
hopelessRomant <satvolume@gmail.com>
2025-11-03 22:08:57 +0000

    struct

--- chapter7/lib/src/lib.rs
+++ chapter7/lib/src/lib.rs
@@ -28,14 +28,27 @@ mod back_of_house{
         //relative path
         super::deliver_order();
     }
+    #[derive(Debug)]
+    pub struct Breakfast {
+        pub drink: String,
+        special: String,
+    }
 
+    impl Breakfast{
+        pub fn monday (drink: &str) -> Breakfast {
+            Breakfast { drink: String::from(drink), special: String::from("red-velvet sunday") }
+        }
+    }   
 }
 
 pub fn eat_at_restraunt() { // since eat_at_restraunt and front_of_house are siblings, front_of_house need not be public
+
     // absolute path
     crate::front_of_house::hosting::add_to_waitlist();
-
     // relative path
     front_of_house::hosting::add_to_waitlist();
 
-}
+    let mut meal = crate::back_of_house::Breakfast::monday("chai");
+    meal.drink = String::from("coffe");
+    print!("{:#?}",meal);
+}   
