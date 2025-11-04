1659bf212d8bc48648f1c7eb1b310029403030c7
hopelessRomant <satvolume@gmail.com>
2025-11-04 09:51:36 +0000

    cleaning

--- chapter7/lib/src/main.rs
+++ chapter7/lib/src/main.rs
@@ -1,12 +1,11 @@
 use lib::back_of_house;
-use lib::back_of_house::Appetizer;
 use lib::eat_at_restraunt;
 
 fn main () {
     let meal = back_of_house::Breakfast::monday("chai");
     eat_at_restraunt();
-    print!("{:#?}\n", meal); // the meal in the 'eat_at_restraunt function and the 'meal' defined in main are different things.
+    print!("{:#?}\n", meal); // the meal in the 'eat_at_restraunt function and the 'meal' defined in main have different scopes.
 
-    let starter = Appetizer::Salad;
+    let starter = back_of_house::Appetizer::Salad;
     println!("Starter order : {:?}", starter);
 }
