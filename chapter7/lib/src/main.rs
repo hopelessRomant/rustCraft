d9053488a4a77cafbee0dec05c04e81371a181e1
hopelessRomant <satvolume@gmail.com>
2025-11-04 10:16:51 +0000

    nested imports

--- chapter7/lib/src/main.rs
+++ chapter7/lib/src/main.rs
@@ -1,11 +1,11 @@
-use lib::back_of_house as kitchen;
+use lib::back_of_house::{Appetizer, Breakfast};
 use lib::eat_at_restraunt as food;
 
 fn main () {
-    let meal = kitchen::Breakfast::monday("chai");
+    let meal = Breakfast::monday("chai");
     food();
     print!("{:#?}\n", meal); // the meal in the 'eat_at_restraunt function and the 'meal' defined in main have different scopes.
 
-    let starter = kitchen::Appetizer::Salad;
+    let starter = Appetizer::Salad;
     println!("Starter order : {:?}", starter);
 }
