94797495c3d8b9a76657b5b3172ed47892e2d21c
hopelessRomant <satvolume@gmail.com>
2025-11-04 10:12:55 +0000

    pub use

--- chapter7/lib/src/lib.rs
+++ chapter7/lib/src/lib.rs
@@ -15,8 +15,7 @@ mod front_of_house {
     }
 }
 
-#[allow(dead_code)]
-fn deliver_order() {}
+pub fn deliver_order() {}
 
 #[allow(dead_code)]
 pub mod back_of_house{
@@ -47,12 +46,16 @@ pub mod back_of_house{
     }   
 }
 
+pub use front_of_house::hosting::add_to_waitlist; // adding to all the scopes.
+
 pub fn eat_at_restraunt() { // since eat_at_restraunt and front_of_house are siblings, front_of_house need not be public
 
     // absolute path
     crate::front_of_house::hosting::add_to_waitlist();
     // relative path
     front_of_house::hosting::add_to_waitlist();
+    // calling from global scope
+    add_to_waitlist();
 
     let mut meal = crate::back_of_house::Breakfast::monday("chai");
     meal.drink = String::from("coffe");
--- chapter7/lib/src/main.rs
+++ chapter7/lib/src/main.rs
@@ -1,9 +1,9 @@
 use lib::back_of_house;
-use lib::eat_at_restraunt;
+use lib::eat_at_restraunt as food;
 
 fn main () {
     let meal = back_of_house::Breakfast::monday("chai");
-    eat_at_restraunt();
+    food();
     print!("{:#?}\n", meal); // the meal in the 'eat_at_restraunt function and the 'meal' defined in main have different scopes.
 
     let starter = back_of_house::Appetizer::Salad;
