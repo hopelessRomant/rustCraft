645294556d67dce6155e5ecc27e10a893de5acf9
hopelessRomant <satvolume@gmail.com>
2025-11-07 08:32:23 +0000

    get method

--- chapter8/src/part1.rs
+++ chapter8/src/part1.rs
@@ -1,4 +1,4 @@
-// Storing Lists of Values with Vectors
+// Staring Lists of Values with Vectors
 pub fn part1() {
     let mut v: Vec<i32> = Vec::new();
     let list = vec![1,2,3];
@@ -6,6 +6,12 @@ pub fn part1() {
     v.push(5);
     v.push(4);
 
-    let one = &list[0];
-    println!("first element of list is {one}");
+    // let one = &list[3];
+    // println!("first element of list is {one}");
+
+    let first = list.get(3); // .get() method retruns an options<T> enum to handle None case.
+    match first {
+        Some(first) => println!("requested element: {}", first), 
+        None => println!("element does not exist"),
+    }
 }
