424b426ee034f9b871082dfa41afb23985445e50
hopelessRomant <satvolume@gmail.com>
2025-11-10 19:50:18 +0000

    team making

--- chapter8/src/practice.rs
+++ chapter8/src/practice.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::io::{self};
 
 pub fn problem1() {
     let mut names: HashMap<String,String> = HashMap::new();
@@ -6,4 +7,25 @@ pub fn problem1() {
     let pointer = names.entry("sahil".to_string()).or_insert("electronics".to_string());
     *pointer = "electronics".to_string();
     println!("{:#?}",names);
+}
+
+pub fn company () {
+    let mut sales_team = sales();
+    let mut members: Vec<String> = Vec::new();
+    let mut department: HashMap<String,Vec<String>> = HashMap::new();
+
+    for i in sales_team.split_whitespace() {
+        
+    }
+}
+
+fn sales() -> String {
+    let mut sales = String::new();
+    println!("Enter the member names for Sales department");
+
+    io::stdin()
+    .read_line(&mut sales)
+    .expect("input not read");
+
+    return sales;
 }
