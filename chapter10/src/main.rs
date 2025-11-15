31f0e35f960caadc5c1ce7c67cab65d7916ced61
hopelessRomant <satvolume@gmail.com>
2025-11-15 19:59:08 +0000

    methods on restricted types

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -2,6 +2,7 @@
 mod part1;
 
 fn main() {
-    let loc = part1::Point{x:10, y:15};
+    let loc = part1::Point{x:10.0, y:15.0};
     println!("location is ({},{})",loc.x,loc.y);
+    println!("mod of the point is: {}", loc.distance());
 }
--- chapter10/src/part1.rs
+++ chapter10/src/part1.rs
@@ -1,5 +1,10 @@
-pub struct Point<T> {
+pub struct Point<T,M> {
     pub x: T,
-    pub y: T,
+    pub y: M,
 }
 
+impl Point<f32, f32> {
+    pub fn distance(&self) -> f32 {
+        ((self.x.powi(2)) + (self.y.powi(2))).sqrt()
+    }
+}
