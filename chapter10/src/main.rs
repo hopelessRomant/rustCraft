03738cca46ac763ef265c5fa67b1c33f27918a00
hopelessRomant <satvolume@gmail.com>
2025-11-16 05:51:44 +0000

    traits

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,8 +1,11 @@
 #[allow(dead_code)]
-mod part1;
+mod generic;
+
+#[allow(dead_code)]
+mod traits;
 
 fn main() {
-    let loc = part1::Point{x:10.0, y:15.0};
+    let loc = generic::Point{x:10.0, y:15.0};
     println!("location is ({},{})",loc.x,loc.y);
     println!("mod of the point is: {}", loc.distance());
 }
