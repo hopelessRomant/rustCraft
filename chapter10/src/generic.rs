fa1f2b99ef2ec9fbce47b2710c9cb616c1c26465
hopelessRomant <satvolume@gmail.com>
2025-11-20 18:45:43 +0000

    cross compairing

--- chapter10/src/generic.rs
+++ chapter10/src/generic.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub struct Point<T,M> {
     pub x: T,
     pub y: M,
@@ -6,5 +8,19 @@ pub struct Point<T,M> {
 impl Point<f32, f32> {
     pub fn distance(&self) -> f32 {
         ((self.x.powi(2)) + (self.y.powi(2))).sqrt()
+    }
+}
+
+impl<T,M> Point<T,M>
+where 
+    T: Display + PartialOrd<M>,
+    M: Display + PartialOrd<T>,
+{
+    pub fn cmp(&self) {
+        if self.x >= self.y {
+            println!("Largest number is {}", self.x);
+        } else {
+            println!("Largest number is {}", self.y);
+        }
     }
 }
