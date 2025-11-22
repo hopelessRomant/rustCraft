27a0df5a91f57d24bee9090db0a8d9bef778c212
hopelessRomant <satvolume@gmail.com>
2025-11-22 18:44:04 +0000

    str usage in functions

---
+++ chapter10/src/lifetime.rs
@@ -0,0 +1,26 @@
+pub fn scope() {
+    let r;
+    {
+        let x =3;
+        r = &x;
+        println!("{:#?}", r);
+    }
+    // println!{"{:#?}", r};
+}
+
+pub fn slices() {
+    let s1 = "pirate".to_string();
+    let s2 = "bay";
+
+    // always a nice idea to use 'str' references in the function that can take both Strings and str
+    let result = longest(s1.as_str(), s2);
+    println!("the longest string is: {}", result);
+}
+
+fn longest(s1: &str, s2:&str) -> String {
+    if s1 >= s2 {
+        s1.to_string()
+    } else {
+        s2.to_string()
+    }
+}
--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -4,25 +4,27 @@ mod generic;
 #[allow(dead_code)]
 mod traits;
 
+#[allow(dead_code)]
+mod lifetime;
+
 // use traits::{Summary, NewsAtricle};
 
 fn main() {
     // --- generic ---
+
     // let loc = generic::Point{x:10.0, y:15.0};
     // println!("location is ({},{})",loc.x,loc.y);
     // println!("mod of the point is: {}", loc.distance());
     // loc.cmp();
 
     // --- traits ---
+
     // let news1 = NewsAtricle::data("World is going to end !!!".to_string());
     // println!("latest news of the day: {}\n presented by: {}", news1.headline(), news1.authors());
 
     // --- lifetime ---
-    let r;
-    {
-        let x =3;
-        r = &x;
-        println!("{:#?}", r);
-    }
+
+    lifetime::scope();
+    lifetime::slices();
 
 }
