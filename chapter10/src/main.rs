369096d27768dfa91e973cf554d1cbd1832a1591
hopelessRomant <satvolume@gmail.com>
2025-11-22 17:50:15 +0000

    ownership doubts

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -4,14 +4,25 @@ mod generic;
 #[allow(dead_code)]
 mod traits;
 
-use traits::{Summary, NewsAtricle};
+// use traits::{Summary, NewsAtricle};
 
 fn main() {
+    // --- generic ---
     // let loc = generic::Point{x:10.0, y:15.0};
     // println!("location is ({},{})",loc.x,loc.y);
     // println!("mod of the point is: {}", loc.distance());
     // loc.cmp();
 
-    let news1 = NewsAtricle::data("World is going to end !!!".to_string());
-    println!("latest news of the day: {}\n presented by: {}", news1.headline(), news1.authors());
+    // --- traits ---
+    // let news1 = NewsAtricle::data("World is going to end !!!".to_string());
+    // println!("latest news of the day: {}\n presented by: {}", news1.headline(), news1.authors());
+
+    // --- lifetime ---
+    let r;
+    {
+        let x =3;
+        r = &x;
+        println!("{:#?}", r);
+    }
+
 }
