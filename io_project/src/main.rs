36ac4bdcfc03760f9148e88da672c2963610663c
hopelessRomant <satvolume@gmail.com>
2025-11-30 19:06:40 +0000

    query and path entry

--- io_project/src/main.rs
+++ io_project/src/main.rs
@@ -2,5 +2,9 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let target = &args[1];
+    let path = &args[2];
+
+    println!("target string is {target}");
+    println!("at path {path}");
 }
