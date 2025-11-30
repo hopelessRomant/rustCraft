fd12b44b6cbbab2f6bb75b19b09ea7195bbc4a8f
hopelessRomant <satvolume@gmail.com>
2025-11-30 19:23:49 +0000

    reading the file

--- io_project/src/main.rs
+++ io_project/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let target = &args[1];
     let path = &args[2];
 
-    println!("target string is {target}");
-    println!("at path {path}");
+    println!("target string is '{target}'");
+    println!("at path '{path}'");
+
+    let content = fs::read_to_string(path).expect("unable to read the file");
+    println!("with the content:\n{}", content);
 }
