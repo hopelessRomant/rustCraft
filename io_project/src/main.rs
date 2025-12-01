d044c9ce241bee580435ec661f874d2b43a64d9e
hopelessRomant <satvolume@gmail.com>
2025-12-01 18:14:10 +0000

    search begin

--- io_project/src/main.rs
+++ io_project/src/main.rs
@@ -3,18 +3,17 @@ use std::{process,env};
 #[allow(dead_code)]
 mod grab;
 
+#[allow(dead_code)]
+mod search;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = grab::Input::build(&args).unwrap_or_else(|err| {
         println!("Input Error: {err}");
         process::exit(1);
     });
-    let content = grab::file_data(&config.path).unwrap_or_else(|err| {
+    let _content = grab::file_data(&config.path).unwrap_or_else(|err| {
         println!("File Error: {err}");
         process::exit(1);
     });
-    
-    println!("target string is '{}'", config.query);
-    println!("at path '{}'", config.path);
-    println!("with the content:\n{}", content);
 }
