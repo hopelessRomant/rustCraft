c12cfd4f4205def6c400ba501c847ad31041257b
hopelessRomant <satvolume@gmail.com>
2025-12-02 09:30:21 +0000

    search

--- io_project/src/search.rs
+++ io_project/src/search.rs
@@ -1,5 +1,11 @@
-pub fn search<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results 
 }
 
 #[cfg(test)]
@@ -10,10 +16,10 @@ mod tests {
     fn search_result() {
         let query = "is";
         let contents = "\
-        Rust:
-        Hello!
-        my name is Sahil Singh
-        nice to meet you.";
+Rust:
+Hello!
+my name is Sahil Singh
+nice to meet you.";
 
         assert_eq!(vec!["my name is Sahil Singh"], search(query, contents));
     }
