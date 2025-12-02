e1b736d37a4a81d5ad9c446fc1663f5d23d21a7c
hopelessRomant <satvolume@gmail.com>
2025-12-02 01:33:55 +0000

    dry test

--- io_project/src/search.rs
+++ io_project/src/search.rs
@@ -1,3 +1,20 @@
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+pub fn search<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
     unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_result() {
+        let query = "is";
+        let contents = "\
+        Rust:
+        Hello!
+        my name is Sahil Singh
+        nice to meet you.";
+
+        assert_eq!(vec!["my name is Sahil Singh"], search(query, contents));
+    }
 }
