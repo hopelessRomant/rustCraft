9977a1184ac7656109234282ea0ed5f1177f51e6
hopelessRomant <satvolume@gmail.com>
2025-12-04 09:42:59 +0000

    T-shirt comany on

--- chapter13/src/main.rs
+++ chapter13/src/main.rs
@@ -4,6 +4,19 @@ mod store;
 use store::*;
 
 fn main () {
-    let _stock = Inventory::build(14, 13, 16);
+    let stock = Inventory::build(14, 13, 16);
+    let user1 = Some(Tshirts::Red);
+    let user2 = None;
 
+    let giveaway1 = stock.give_away(user1);
+    let giveaway2 = stock.give_away(user2);
+
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user1.unwrap(), giveaway1
+    );
+    println!(
+        "The user with no preference gets {:?}",
+        giveaway2
+    );
 }
