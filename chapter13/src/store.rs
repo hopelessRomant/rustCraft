f5a064b57df3e297a6a1d17e812e1f6735dac1ed
hopelessRomant <satvolume@gmail.com>
2025-12-04 07:55:39 +0000

    method3: Type alias + trait definition

--- chapter13/src/store.rs
+++ chapter13/src/store.rs
@@ -1,19 +1,26 @@
-use std::collections::HashMap;
+use std::{collections::HashMap};
 
+pub trait Event {
+    fn give_away(&self, choice: Option<Tshirts>) -> Tshirts;
+    fn most_stocked(&self) -> Tshirts;
+}
+
+pub type Inventory = HashMap<Tshirts, i32>;
+
+#[derive(Debug, Clone, Copy)]
 pub enum Tshirts {
     Red,
     Blue,
     Green,
 }
 
-pub struct Inventory {
-    stock: HashMap<String, i32>
-}
-
-impl Inventory {
-    pub fn give_away(self, _choice: Option<Tshirts>) {
-        // choice.unwrap_or_else(|| self.default())
+impl Event for Inventory {
+    fn give_away(&self, choice: Option<Tshirts>) -> Tshirts {
+        choice.unwrap_or_else(|| self.most_stocked())
     }
 
-    fn default() {}
+    fn most_stocked(&self) -> Tshirts {
+        let gift = self.iter().max_by_key(|(_, v)| *v).map(|(k, _)| *k);
+        gift.unwrap()
+    }
 }
