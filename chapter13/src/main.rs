7071157fd437ac6e9aa180bf83994059fb655544
hopelessRomant <satvolume@gmail.com>
2025-12-04 19:26:09 +0000

    moving ownershipt

--- chapter13/src/main.rs
+++ chapter13/src/main.rs
@@ -1,22 +1,22 @@
 #[allow(dead_code)]
 mod store;
 
-use store::*;
+// use store::*;
 
 fn main () {
-    let stock = Inventory::build(14, 13, 16);
-    let user1 = Some(Tshirts::Red);
-    let user2 = None;
+    // let stock = Inventory::build(14, 13, 16);
+    // let user1 = Some(Tshirts::Red);
+    // let user2 = None;
 
-    let giveaway1 = stock.give_away(user1);
-    let giveaway2 = stock.give_away(user2);
+    // let giveaway1 = stock.give_away(user1);
+    // let giveaway2 = stock.give_away(user2);
 
-    println!(
-        "The user with preference {:?} gets {:?}",
-        user1.unwrap(), giveaway1
-    );
-    println!(
-        "The user with no preference gets {:?}",
-        giveaway2
-    );
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let only_borrows = || println!("From closure: {list:?}");
+
+    println!("Before calling closure: {list:?}");
+    only_borrows();
+    println!("After calling closure: {list:?}");
 }
