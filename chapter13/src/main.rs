1533866b64df794835f02132fa4f542fc4bdb187
hopelessRomant <satvolume@gmail.com>
2025-12-05 21:19:14 +0000

    iterators init

--- chapter13/src/main.rs
+++ chapter13/src/main.rs
@@ -4,17 +4,21 @@ mod store;
 #[allow(dead_code)]
 mod closure;
 
+#[allow(dead_code)]
+mod iterators;
+
 // use store::*;
 
 fn main () {
     // let stock = Inventory::build(14, 13, 16);
     // let user1 = Some(Tshirts::Red);
     // let user2 = None;
-
     // let giveaway1 = stock.give_away(user1);
     // let giveaway2 = stock.give_away(user2);
 
     // closure::reff();
     // closure::thread();
-    closure::fnmut_trait();
+    // closure::fnmut_trait();
+
+    
 }
