8feed3f511c34d2673521718158407eef6898471
romanticNomad <satvolume@gmail.com>
2025-12-07 11:03:08 +0000

    impl Deref trait

--- chapter15/src/boxt.rs
+++ chapter15/src/boxt.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 pub fn basic() {
     let x = Box::new(5);
     let y =x;
@@ -15,4 +17,30 @@ use crate::boxt::List::{Cons, Nil};
 pub fn cons_list() {
     let x = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("{x:?}")
+}
+
+pub struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    pub fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+// implimenting 'Deref' trait for the MyBox struct tuple
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+#[test]
+pub fn deref() {
+    let x = MyBox::new("Chrysanthemum");
+    let y = "Chrysanthemum";
+
+    assert_eq!(y, *x); // Deref trait returns the reference to "Chrysanthemum"
 }
