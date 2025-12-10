09071647a6837b2c57078bf0c4601ba434e9507f
romanticNomad <satvolume@gmail.com>
2025-12-10 21:11:18 +0000

    move keyword

--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -2,5 +2,6 @@
 mod thread;
 
 fn main() {
-    thread::thread_test();
+    // thread::thread_test();
+    thread::move_ownership();
 }
--- chapter16/src/thread.rs
+++ chapter16/src/thread.rs
@@ -9,14 +9,21 @@ pub fn thread_test () {
         }
     });
 
-    handle.join().unwrap();
-
     for i in 1..5 {
         print!("main count: {i} ");
         thread::sleep(Duration::from_millis(1));
     }
-
-   
+    
+    handle.join().unwrap();
 }
 
+pub fn move_ownership() {
+    let v = vec![2,3,5,7];
 
+    // we use the move key word to move the ownership of values into the closure environment.
+    let handle = thread::spawn(move || {
+        println!("this value is now owned by the thread:\n {:?}", v)
+    });
+
+    handle.join().unwrap();
+}
