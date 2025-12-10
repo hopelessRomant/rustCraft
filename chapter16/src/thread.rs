cefa345f58db7892dea50620261c081025342d3d
romanticNomad <satvolume@gmail.com>
2025-12-10 21:05:53 +0000

    join handle restriction

--- chapter16/src/thread.rs
+++ chapter16/src/thread.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 pub fn thread_test () {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("thread count: {i}");
             thread::sleep(Duration::from_millis(1));
@@ -13,4 +13,8 @@ pub fn thread_test () {
         print!("main count: {i} ");
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
+
+
