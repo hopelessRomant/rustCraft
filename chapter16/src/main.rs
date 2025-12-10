f15f9f3c0c37f4fdeae763c42f14a6a1af1212f0
romanticNomad <satvolume@gmail.com>
2025-12-10 19:15:33 +0000

    thread and main pareller run

--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -1,3 +1,6 @@
+#[allow(dead_code)]
+mod thread;
+
 fn main() {
-    println!("Hello, world!");
+    thread::thread_test();
 }
---
+++ chapter16/src/thread.rs
@@ -0,0 +1,16 @@
+use std::thread;
+use std::time::Duration;
+
+pub fn thread_test () {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("thread count: {i}");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        print!("main count: {i} ");
+        thread::sleep(Duration::from_millis(1));
+    }
+}
