7141a4b686063cbad0e6d19385ad495dca00e194
romanticNomad <satvolume@gmail.com>
2025-12-12 21:16:10 +0000

    Future trait

--- chapter17/src/future.rs
+++ chapter17/src/future.rs
@@ -7,3 +7,15 @@ pub async fn page_title(url: &str) -> Option<String> {
         .select_first("title")
         .map(|title| title.inner_html() )
 }
+
+// Alternate version of the above code; "what is meant by 'async' for the compiler"
+// fn page_title(url: &str) -> impl Future<Output = Option<String>> {
+//     async move {
+//         let text = trpl::get(url).await.text().await;
+//         Html::parse(&text)
+//             .select_first("title")
+//             .map(|title| title.inner_html())
+//     }
+// }
+
+
